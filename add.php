4262ae2c6b879f8d7e527c2bcbf22eb30689ad71
aref-ivan <aref.ivan@gmail.com>
2022-07-20 16:35:49 +0300

    remove vars title content in html

--- add.php
+++ add.php
@@ -27,15 +27,17 @@
 		}
 	}
 	else{
+		$title = '';
+		$content ='';
 		$msg = '';
 	}
 	
 ?>
 <form method="post">
 	Название<br>
-	<input type="text" name="title" value = "<?= $title??''?>"><br>
+	<input type="text" name="title" value = "<?= $title?>"><br>
 	Контент<br>
-	<textarea name="content"><?=$content??''?></textarea><br>
+	<textarea name="content"><?=$content?></textarea><br>
 	<input type="submit" value="Добавить">
 </form>
 <?php echo $msg; ?>
