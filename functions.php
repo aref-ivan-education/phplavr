f563c80af82ca19e29ba4c5388ddab19ad7916f7
aref-ivan <aref.ivan@gmail.com>
2022-07-22 19:57:14 +0300

    save functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
-    $user=[  
-        "login" => "aris",
-        "password" => "Terrika"]
-    ;
+
     function isAuth($user){
         session_start();
         $isAuth=FALSE;
@@ -29,4 +26,34 @@
     function myHash($str){
 		return hash('sha256', $str . 'erwdsddsdsds');
    }
+
+   function db_connect(){
+    static $db;
+
+    if($db==null){
+        $db = new PDO('mysql:host=localhost;dbname=phplavr_news', 'root' , '');
+        $db->exec('SET NAMES UTF8');
+    }
+    return $db;
+   }
+   function db_query($sql, $params = []){
+        $db = db_connect();
+        
+        $query = $db->prepare($sql);
+        $query->execute($params);
+        
+        db_check_error($query);
+        
+        return $query;
+    }
+
+    function db_check_error($query){
+        $info = $query->errorInfo();
+
+        if($info[0] != PDO::ERR_NONE){
+            exit($info[2]);
+        }
+    }
+
+
 ?>
