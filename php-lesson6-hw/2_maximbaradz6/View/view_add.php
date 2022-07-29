75cfb67d72dabbd7f3bf480379620681279d8668
aref-ivan <aref.ivan@gmail.com>
2022-07-29 22:10:44 +0300

    template + одна точка входа no hw6

--- php-lesson6-hw/2_maximbaradz6/View/view_add.php
+++
@@ -1,9 +0,0 @@
-<p style="color:red"><?=$err?></p>
-<form method="post">
-            <i>Название</i><br>                    
-                 <input type="text" name="title" value= "<?=$title?>"><br>
-            <i>Контент</i><br>
-                    <textarea name="content" placeholder="<?=$content?>"></textarea><br>
-                    <input type="submit" value="Добавить">
-        </form>
-        <strong><?= $msg, "<br> <a href= main.php><img src='image/main.png' alt='На главную'></a>"?></strong>
--- php-lesson6-hw/2_maximbaradz6/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-    include_once("Model/articles.php");
-    include_once("Model/auth.php");
-    $articles = show_accept_articles();
-    count($articles);    
-    unsetSesCook();
-    include("View/view_index.php"); 
-?>
